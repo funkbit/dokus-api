a4eddeb55164a6c1b79787b9094cd1af2018cd04
Jon Lønne <jon@funkbit.no>
2012-06-21 11:12:32 +0200

    Made the Python package installable.

---
+++ python/README.md
@@ -0,0 +1,18 @@
+# pydokus
+
+pydokus is Python module for interacting with the Dokus REST API. Dokus is a 
+Norwegian web app for invoicing and timetracking, see https://dokus.no for 
+more information.
+
+## Installation
+
+Install `pydokus`:
+
+	pip install pydokus
+
+Alternatively, download the source code and manually add it to your `PYTHONPATH`.
+
+## Usage
+
+See the `sample.py` file for usage examples. For further documentation, please
+refer to the [GitHub Wiki](https://github.com/funkbit/dokus-api/wiki).
---
+++ python/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from dokus import __version__
+from setuptools import setup
+
+# Publish to Pypi
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='pydokus',
+    version=__version__,
+    description='pydokus is Python module for interacting with the Dokus REST API.',
+    long_description=open('README.md').read(),
+    author='Funkbit AS',
+    author_email='post@funkbit.no',
+    url='https://github.com/funkbit/dokus-api',
+    packages=['dokus',],
+    license='BSD',
+    classifiers = (
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    )
+)
