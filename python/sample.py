43928003e582a6bd300afd0dc6b44cdae859dc4e
Jon Lønne <jon@funkbit.no>
2012-06-21 11:03:21 +0200

    Renamed import in sample file.

--- python/sample.py
+++ python/sample.py
@@ -30,7 +30,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 from datetime import date, timedelta
 from decimal import Decimal
 
-from pydokus import DokusService, DokusObject
+from dokus import DokusService, DokusObject
 
 # Replace email, password and subdomain with your proper credentials
 service = DokusService(email='username@example.com', password='password', subdomain='mycompany')
